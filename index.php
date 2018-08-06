60f1bc419d0cf5a6c8d3e81dd2e3f40c7c7226e5
Duncan Naves <duncan@platform.sh>
2018-08-06 11:44:05 +0200

    first

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Votre site n'est pas encore construit. N'oubliez pas de pousser le code que vous souhaitez utiliser pour ce faire.</p>'; ?> 
+ </body>
+</html>
