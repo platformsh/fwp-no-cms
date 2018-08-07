31f86c7ffa732513668e95d40f8771f5f3493836
NDuncan <9960508+DuncanNaves@users.noreply.github.com>
2018-08-07 13:45:42 +0200

    Fix typo in index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Votre site n'est pas encore construit. N'oubliez pas de pousser le code que vous souhaitez utiliser pour ce faire.</p>'; ?> 
+ <?php echo '<p>Votre site n\'est pas encore construit. N\'oubliez pas de pousser le code que vous souhaitez utiliser pour ce faire.</p>'; ?> 
  </body>
 </html>
